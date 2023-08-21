51c197203db4bee6d8d276cfe606fb13b9996341
jhonyso <jhonysds2@gmail.com>
2023-08-21 12:09:56 -0300

    test

--- src/services/persistency.ts
+++ src/services/persistency.ts
@@ -4,4 +4,7 @@ export class Persistency implements PersistencyProtocol {
   saveOrder(): void {
     console.log('Pedido salvo com sucesso...');
   }
+  SaveOrder(): void {
+    console.log('Pedido salvo com sucesso...');
+  }
 }
